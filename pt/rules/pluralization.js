d9d8597774c2dc13b85892db22486f8c1d3fca1a
Lucas Mordzin <mordzin@gmail.com>
2024-08-02 03:39:49 -0300

    Fix pt pluralization

--- pt/rules/pluralization.js
+++ pt/rules/pluralization.js
@@ -3,5 +3,11 @@
  */
 
 export const pluralizationRules = [
-  // Incluir as regras de pluralização aqui
+  [/(ão)$/i, '$1es'], // Ex: "cão" -> "cães"
+  [/(r|z)$/i, '$1es'], // Ex: "flor" -> "flores", "arroz" -> "arrozes"
+  [/(m)$/i, '$1ns'], // Ex: "homem" -> "homens"
+  [/(l)$/i, '$1is'], // Ex: "animal" -> "animais"
+  [/(s|x)$/i, '$1'], // Ex: "lápis" -> "lápis", "tórax" -> "tórax"
+  [/([aeiou])$/i, '$1s'], // Ex: "banana" -> "bananas", "carro" -> "carros"
+  [/ã$/i, 'ãs'], // Ex: "maçã" -> "maçãs"
 ]
